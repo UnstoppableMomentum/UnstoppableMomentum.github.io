91f3598a3e71a5d4590a0dc70889095e712425aa
Serg <sobolentsev@gmail.com>
2022-06-01 03:40:43 +0300

    v1.1.0

--- test-peer-server/ui.js
+++
@@ -1,8 +0,0 @@
-
-function initUi() {
-    document.getElementById("server").value = config.URL_SERVER_HTTPS;
-}
-
-function showCmdResult(data) {
-    document.getElementById("cmdResult").value = data;
-}
